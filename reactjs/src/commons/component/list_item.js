ad2ac4f9963de779ddcd48978d963febc63ea34c
Ryula <hobby519@naver.com>
2022-02-14 03:28:24 +0900

    like

--- reactjs/src/commons/component/list_item.js
+++ reactjs/src/commons/component/list_item.js
@@ -1,18 +1,34 @@
 import '../../assets/list_item.css';
+import Like from './list_like';
+import {AiOutlineShoppingCart} from 'react-icons/ai';
 
 function Litem({items}){
+    
     return(
         <div id="items">
             {
                 items.map((item)=>{
-                    const {id, title, des, price, type, category, img}=item;
+                    const {id, title, des, price, type, img}=item;
+
+                    const setType=(type)=>{
+                        return(
+                            type.map((e)=> <img id="img" src={`/img/icon_${e}.png`} alt={e}/>)
+                        )
+                        
+                       
+                    }
                     return (
                         <div id="item" key={id}>
                             <img src={img} alt={title}/>
+                            <div id="icon">
+                                <Like/>
+                                <AiOutlineShoppingCart color="#989898" size="30px"/>
+                            </div>
                             <div id="des">
+                                <span>{setType(type)}</span>
                                 <span className="item_title">{title}</span>
                                 <span className="item_des">{des}</span>
-                                <span className="item_price">{price}</span>
+                                <span className="item_price">₩ {price}</span>
                             </div>
                         </div>          
                     )
